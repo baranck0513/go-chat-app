48c4bf1f6eed73daa8a6f6039d9b7f4a72a6f4d9
baranck0513 <173565820+baranck0513@users.noreply.github.com>
2025-11-23 23:49:05 +0000

    Initial commit - Go Chat App

---
+++ frontend/src/components/Message/Message.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./Message.scss";
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: this.props.message
+    };
+  }
+
+  render() {
+    return <div className="Message">{this.state.message.body}</div>;
+  }
+}
+
+export default Message;
